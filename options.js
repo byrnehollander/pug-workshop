d2791e0ce252d216c1636b40fec395e1273e2b79
Byrne Hollander <bhollander823@gmail.com>
2017-04-25 14:58:42 -0400

    done -- all roads lead to armin

--- options.js
+++ options.js
@@ -1,46 +1,46 @@
 exports.doctype = 'html';
 
-exports.happy = true;
+exports.happy = false;
 
 exports.sitemap = [
   {
     header: 'Category A',
     links: [
-      { name: 'Link A.1', url: '#' },
-      { name: 'Link A.2', url: '#' },
-      { name: 'Link A.3', url: '#' },
-      { name: 'Link A.4', url: '#' },
-      { name: 'Link A.5', url: '#' }
+      { name: 'Link A.1', url: 'https://www.youtube.com/watch?v=YoJ9wZjogz8' },
+      { name: 'Link A.2', url: 'https://www.youtube.com/watch?v=YoJ9wZjogz8' },
+      { name: 'Link A.3', url: 'https://www.youtube.com/watch?v=YoJ9wZjogz8' },
+      { name: 'Link A.4', url: 'https://www.youtube.com/watch?v=YoJ9wZjogz8' },
+      { name: 'Link A.5', url: 'https://www.youtube.com/watch?v=YoJ9wZjogz8' }
     ],
   },
   {
     header: 'Category B',
     links: [
-      { name: 'Link B.1', url: '#' },
-      { name: 'Link B.2', url: '#' },
-      { name: 'Link B.3', url: '#' },
-      { name: 'Link B.4', url: '#' },
-      { name: 'Link B.5', url: '#' }
+      { name: 'Link B.1', url: 'https://www.youtube.com/watch?v=YoJ9wZjogz8' },
+      { name: 'Link B.2', url: 'https://www.youtube.com/watch?v=YoJ9wZjogz8' },
+      { name: 'Link B.3', url: 'https://www.youtube.com/watch?v=YoJ9wZjogz8' },
+      { name: 'Link B.4', url: 'https://www.youtube.com/watch?v=YoJ9wZjogz8' },
+      { name: 'Link B.5', url: 'https://www.youtube.com/watch?v=YoJ9wZjogz8' }
     ],
   },
   {
     header: 'Category C',
     links: [
-      { name: 'Link C.1', url: '#' },
-      { name: 'Link C.2', url: '#' },
-      { name: 'Link C.3', url: '#' },
-      { name: 'Link C.4', url: '#' },
-      { name: 'Link C.5', url: '#' }
+      { name: 'Link C.1', url: 'https://www.youtube.com/watch?v=YoJ9wZjogz8' },
+      { name: 'Link C.2', url: 'https://www.youtube.com/watch?v=YoJ9wZjogz8' },
+      { name: 'Link C.3', url: 'https://www.youtube.com/watch?v=YoJ9wZjogz8' },
+      { name: 'Link C.4', url: 'https://www.youtube.com/watch?v=YoJ9wZjogz8' },
+      { name: 'Link C.5', url: 'https://www.youtube.com/watch?v=YoJ9wZjogz8' }
     ],
   },
   {
     header: 'Category D',
     links: [
-      { name: 'Link D.1', url: '#' },
-      { name: 'Link D.2', url: '#' },
-      { name: 'Link D.3', url: '#' },
-      { name: 'Link D.4', url: '#' },
-      { name: 'Link D.5', url: '#' }
+      { name: 'Link D.1', url: 'https://www.youtube.com/watch?v=YoJ9wZjogz8' },
+      { name: 'Link D.2', url: 'https://www.youtube.com/watch?v=YoJ9wZjogz8' },
+      { name: 'Link D.3', url: 'https://www.youtube.com/watch?v=YoJ9wZjogz8' },
+      { name: 'Link D.4', url: 'https://www.youtube.com/watch?v=YoJ9wZjogz8' },
+      { name: 'Link D.5', url: 'https://www.youtube.com/watch?v=YoJ9wZjogz8' }
     ],
   },
 ];
